5d8a89c7506732e7be00ff099fb649fe1fb9318d
iqoOopi <haotian.zhang1989@hotmail.com>
2019-03-08 20:09:15 -0700

    init

--- Lab3_Haotian_Zhang/Form1.cs
+++ Lab3_Haotian_Zhang/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,65 @@ namespace Lab3_Haotian_Zhang
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void productsBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Validate();
+                this.productsBindingSource.EndEdit();
+                this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+            }
+            catch (DBConcurrencyException)
+            {
+                MessageBox.Show("Another user changed data in the meantime, Try again!");
+                this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+            }
+            catch (NoNullAllowedException ex)
+            {
+                MessageBox.Show("Please input data!, Try again!" + ex.Message+ex.Data.Values);
+                
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Database error while saving data:" + ex.Message);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Other error while saving data: " + ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.categoriesTableAdapter.Fill(this.northwindDataSet.Categories);
+                this.suppliersTableAdapter.Fill(this.northwindDataSet.Suppliers);
+                this.order_DetailsTableAdapter.Fill(this.northwindDataSet.Order_Details);
+                this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show("Database error while saving data:" + ex.Message);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Other error while saving data: " + ex.Message);
+            }
         }
     }
 }
